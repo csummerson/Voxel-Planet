c0754bfcbe6de45c60cdc25425a875c590a06d8c
csummerson <cody.summerson@gmail.com>
2025-12-03 01:47:05 -0600

    Prepping structure

---
+++ VoxelPlanet/src/main.js
@@ -0,0 +1,10 @@
+import * as THREE from 'three';
+
+import { SceneManager } from './core/sceneManager.js';
+import { GameLoop } from './core/gameLoop.js';
+import { Input } from './core/input.js';
+
+const sceneManager = new SceneManager();
+const gameLoop = new GameLoop();
+const input = new Input();
+
